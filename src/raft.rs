826731bae1286009ca8495399def5ad393125398
Andrew Halle <me@andrewhalle.dev>
2023-05-20 18:19:41 -0700

    Some `tonic` preliminaries
    **This commit**
    - Update the `AppendEntries` definition so that the command is a
      `oneof`.
    - Add `Service` to the `raft` module.

--- src/raft.rs
+++ src/raft.rs
@@ -1,3 +1,10 @@
+use tonic::{Request, Response, Status};
+
+use rpc::{
+    raft_rpc_service_server::{RaftRpcService, RaftRpcServiceServer},
+    AppendEntriesReply, AppendEntriesRequest,
+};
+
 #[allow(unused)]
 enum State {
     Follower,
@@ -7,4 +14,16 @@ enum State {
 
 mod rpc {
     tonic::include_proto!("raft");
+}
+
+struct Service;
+
+#[tonic::async_trait]
+impl RaftRpcService for Service {
+    async fn append_entries(
+        &self,
+        request: Request<AppendEntriesRequest>,
+    ) -> Result<Response<AppendEntriesReply>, Status> {
+        todo!()
+    }
 }
